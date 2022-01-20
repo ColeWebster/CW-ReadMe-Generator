3eb65b0390ae3a2197ede5753dcae9b8fd3e5abe
Cole Webster <Cole.Webster@gmail.com>
2022-01-20 14:40:32 -0500

    Adding the starter code from end of class

---
+++ index.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+const inquirer = require('inquirer');
+const generateMarkdown = require('.utils/generateMarkdown');
+
+const questions = [];
+
+function writetoFile(filname, data) {}
+
+function init() {}
+
+init();
