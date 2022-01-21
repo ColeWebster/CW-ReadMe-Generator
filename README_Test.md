7b6a191c00079830cef1c8dca74bf4fe99ca49e6
Cole Webster <Cole.Webster@gmail.com>
2022-01-21 09:41:41 -0500

    Added working function and a test readme file

---
+++ README_Test.md
@@ -0,0 +1,8 @@
+ ## About the project
+        - Learn more about Wolves a la Carte below.
+        - This is a test file created with my function.
+
+  ## Links
+        - View our repository here: github.com/https://github.com/WRuback/Wolves-a-la-carte
+        - View our live deployment here: https://wruback.github.io/Wolves-a-la-carte/  
+    
--- index.js
+++ index.js
@@ -1,12 +1,47 @@
 const fs = require('fs');
-const path = require('path');
+// const path = require('path');
 const inquirer = require('inquirer');
-const generateMarkdown = require('.utils/generateMarkdown');
+// const generateMarkdown = require('.utils/generateMarkdown');
 
-const questions = [];
+const generateReadMe = ({ headline , gitLinks , liveLink }) =>
+    ` ## About the project
+        - Learn more about ${headline} below.
 
-function writetoFile(filname, data) {}
+      ## Links
+        - View our repository here: ${gitLinks}
+        - View our live deployment here: ${liveLink}  
+    `
 
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'headline',
+            message: 'Enter your header here:'
+        },
+        {
+            type: 'input',
+            name: 'gitLinks',
+            message: 'Enter your Github repository here:'
+        },
+        {
+            type: 'input',
+            name: 'liveLink',
+            message: 'Enter your live deployments link here:'
+        },
+    ])
+    .then((responses) => {
+        const createReadMe = generateReadMe(responses);
+
+        fs.writeFile('readme.md', createReadMe, (err) =>
+            err ? console.log(err) : console.log('Created!')
+        );
+    });
+
+
+
+    // function writetoFile(filname, data) { }
 // function init() {
 // const init = async () {
 //     inquirer.prompt(questions).then((answers => {
@@ -16,4 +51,3 @@ function writetoFile(filname, data) {}
 // }
 
 
-init();
